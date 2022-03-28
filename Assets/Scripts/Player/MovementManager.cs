6491ba1cc767d9e02a2d5bfbd3943cf7efc0e42c
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-03-28 09:41:26 -0300

    Adding the related directions

--- Assets/Scripts/Player/MovementManager.cs
+++ Assets/Scripts/Player/MovementManager.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class MovementManager : MonoBehaviour{
+
+    Vector3 direction = Vector3.zero;
+    public Vector3 RealPlayerDirection { get { return direction; } }
+    
+    void Start(){
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate(){
+        direction = ObjectRelatedDirection(PlayerManager.instance.InputManager.LeftStickValue, Camera.main.gameObject);
+    }
+
+    Vector3 ObjectRelatedDirection(Vector2 inputDirection, GameObject relatedObject){
+        var forwardDirection = Vector3.ProjectOnPlane(relatedObject.transform.forward, transform.up);
+        var rightDirection = Vector3.ProjectOnPlane(relatedObject.transform.right, transform.up);
+        var finalDirection = forwardDirection * inputDirection.y + rightDirection * inputDirection.x;
+        return finalDirection;
+    }
+
+    void Move(){
         
+    }
+
+    //Debug
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        if(direction != Vector3.zero) Gizmos.DrawLine(transform.position, direction * 5);
     }
 }

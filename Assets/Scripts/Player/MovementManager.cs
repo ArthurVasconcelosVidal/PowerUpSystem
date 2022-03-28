beea3451a85008b9c9e594ed0ca3f64e6be6a92e
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-03-28 10:07:45 -0300

    Impelementing player rotation

--- Assets/Scripts/Player/MovementManager.cs
+++ Assets/Scripts/Player/MovementManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MovementManager : MonoBehaviour{
 
     Vector3 direction = Vector3.zero;
+    [SerializeField] float rotationVelocity;
+
     public Vector3 RealPlayerDirection { get { return direction; } }
     
     void Start(){
@@ -13,17 +15,19 @@ public class MovementManager : MonoBehaviour{
 
     void FixedUpdate(){
         direction = ObjectRelatedDirection(PlayerManager.instance.InputManager.LeftStickValue, Camera.main.gameObject);
+        RotatePlayer();
     }
 
     Vector3 ObjectRelatedDirection(Vector2 inputDirection, GameObject relatedObject){
         var forwardDirection = Vector3.ProjectOnPlane(relatedObject.transform.forward, transform.up);
         var rightDirection = Vector3.ProjectOnPlane(relatedObject.transform.right, transform.up);
         var finalDirection = forwardDirection * inputDirection.y + rightDirection * inputDirection.x;
-        return finalDirection;
+        return finalDirection.normalized;
     }
 
-    void Move(){
-        
+    void RotatePlayer(){
+        var newRotation = Quaternion.FromToRotation(PlayerManager.instance.MeshObject.transform.forward, direction) * PlayerManager.instance.MeshObject.transform.rotation; 
+        PlayerManager.instance.MeshObject.transform.rotation = Quaternion.Lerp(PlayerManager.instance.MeshObject.transform.rotation, newRotation, rotationVelocity * Time.fixedDeltaTime);
     }
 
     //Debug

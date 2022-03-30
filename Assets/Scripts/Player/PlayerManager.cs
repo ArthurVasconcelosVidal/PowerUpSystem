b0049a88d3e1b781a322c25a197ab8dc6afe3d86
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-03-30 09:03:22 -0300

    Adding ActionManager script and including on PlayerManager

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -10,11 +10,13 @@ public class PlayerManager : MonoBehaviour{
     [SerializeField] InputManager inputManager;
     [SerializeField] Rigidbody rigidbody;
     [SerializeField] GameObject meshObject;
+    [SerializeField] ActionManager actionManager;
 
     public MovementManager MovementManager { get{ return movementManager; } }
     public InputManager InputManager { get { return inputManager; } }
     public Rigidbody CharacterRigidbody { get { return rigidbody; } }
     public GameObject MeshObject { get { return meshObject; } }
+    public ActionManager ActionManager { get { return actionManager;} }
     
     private void Awake() {
         if (instance == null) instance = this;

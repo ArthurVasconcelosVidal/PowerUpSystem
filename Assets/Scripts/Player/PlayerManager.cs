c54ab539a0d18a1b168789c0ce5ab7721d59e52a
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-03-30 16:18:43 -0300

    Adding collider propertie and fixing interactive object propertie

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -11,14 +11,20 @@ public class PlayerManager : MonoBehaviour{
     [SerializeField] Rigidbody rigidbody;
     [SerializeField] GameObject meshObject;
     [SerializeField] ActionManager actionManager;
+    [SerializeField] ColliderManager colliderManager;
+    [SerializeField] GameObject interactiveObject;
 
     public MovementManager MovementManager { get => movementManager; }
     public InputManager InputManager { get => inputManager; }
     public Rigidbody CharacterRigidbody { get => rigidbody; }
     public GameObject MeshObject { get => meshObject; }
     public ActionManager ActionManager { get => actionManager; }
-    public GameObject InteractiveObject { get => InteractiveObject; 
-                                          set { if(value.GetComponent<IInteractBehaviour>() != null) InteractiveObject = value; } }
+    public GameObject InteractiveObject { get => interactiveObject; 
+                                          set { if(value != null && value.GetComponent<IInteractBehaviour>() != null)  interactiveObject = value;
+                                                else interactiveObject = null; 
+                                              } 
+                                        }
+    public ColliderManager ColliderManager { get => colliderManager; }
     private void Awake() {
         if (instance == null) instance = this;
         else Destroy(this);

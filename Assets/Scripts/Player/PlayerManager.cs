636a52cdc4203af5b85a303116223561668c2d90
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-03-30 12:55:01 -0300

    Refactoring the code

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -12,12 +12,13 @@ public class PlayerManager : MonoBehaviour{
     [SerializeField] GameObject meshObject;
     [SerializeField] ActionManager actionManager;
 
-    public MovementManager MovementManager { get{ return movementManager; } }
-    public InputManager InputManager { get { return inputManager; } }
-    public Rigidbody CharacterRigidbody { get { return rigidbody; } }
-    public GameObject MeshObject { get { return meshObject; } }
-    public ActionManager ActionManager { get { return actionManager;} }
-    
+    public MovementManager MovementManager { get => movementManager; }
+    public InputManager InputManager { get => inputManager; }
+    public Rigidbody CharacterRigidbody { get => rigidbody; }
+    public GameObject MeshObject { get => meshObject; }
+    public ActionManager ActionManager { get => actionManager; }
+    public GameObject InteractiveObject { get => InteractiveObject; 
+                                          set { if(value.GetComponent<IInteractBehaviour>() != null) InteractiveObject = value; } }
     private void Awake() {
         if (instance == null) instance = this;
         else Destroy(this);

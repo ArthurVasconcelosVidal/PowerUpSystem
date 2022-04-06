93820ae19717b962953c48c38f2a4daf8943fdbc
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-04-06 14:18:35 -0300

    Adding Bow Projectile behaviour

--- Assets/Scripts/PowerUp/PowerUps/Bow/BowProjectile.cs
+++ Assets/Scripts/PowerUp/PowerUps/Bow/BowProjectile.cs
@@ -3,28 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BowProjectile : MonoBehaviour{
-    [SerializeField] GameObject projectilePrefab;
     Rigidbody rigidbody;
     public Vector3 ArrowDirection { get; }
     public float DragForce { get; }
     public float ShootForce { get; }
+    bool init = false;
 
     public BowProjectile(Vector3 arrowDirection, float dragForce, float shootForce){
         ArrowDirection = arrowDirection;
         DragForce = dragForce;
         ShootForce = shootForce;
+        init = true;
     }
 
     // Start is called before the first frame update
     void Start(){
         rigidbody = GetComponent<Rigidbody>();
-        Instantiate(projectilePrefab, transform);
         Debug.Log($"Arrow direction: {ArrowDirection} || Drag Force: {DragForce} || ShootForce: {ShootForce}");
     }
 
     // Update is called once per frame
     void FixedUpdate(){
-        ApplyArrowForce(ArrowDirection, ShootForce, DragForce);
+        if(init)
+            ApplyArrowForce(ArrowDirection, ShootForce, DragForce);
     }
 
     public void ApplyArrowForce(Vector3 arrowDirection, float arrowStartForce, float dragForce){

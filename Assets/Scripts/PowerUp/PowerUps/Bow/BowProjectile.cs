532e8700a1273780fba3f9f869ad7ec36bb629fd
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-04-26 14:22:20 -0300

    Refactoring bezier function

--- Assets/Scripts/PowerUp/PowerUps/Bow/BowProjectile.cs
+++ Assets/Scripts/PowerUp/PowerUps/Bow/BowProjectile.cs
@@ -12,6 +12,7 @@ public class BowProjectile : MonoBehaviour{
     float ShootForce { get; set;}
     bool init = false;
     [SerializeField] Vector4 teste = Vector4.one;
+    Vector3[] arrowBezierPositions = new Vector3[10];
     public void Initialize(Vector3 arrowDirection, MinMaxFloat MinMaxForce, float actualForce){
         ArrowDirection = arrowDirection;
         ShootForce = actualForce;
@@ -21,27 +22,40 @@ public class BowProjectile : MonoBehaviour{
 
         rigidbody = GetComponent<Rigidbody>();
         Invoke("ArrowDecay", timeToDecay);
-        
+        GetAllBezierPositions();
+
         init = true;
         
         Debug.Log($"Lerp Result: {arrowIntensity} || Min value: {MinMaxForce.Min} || Max value: {MinMaxForce.Max} || ActualValue: {actualForce}");
         Debug.Log($"Time to Decay: {timeToDecay}");
     }
 
+    void GetAllBezierPositions(){
+        float time = 0;
+
+        Vector3 firstPoint = transform.position;
+        Vector3 secondPoint = transform.position + (transform.forward * 13);
+        Vector3 thirdPoint = transform.position + (transform.forward * 13) - (transform.up * 3);
+        
+        for (int i = 0; i < arrowBezierPositions.Length; i++){
+            arrowBezierPositions[i] = PositionAtQuadraticBezierCurve(time, firstPoint, secondPoint, thirdPoint);
+            time += 0.1f;
+        }
+    }
 
     void ArrowDecay() => actualDragForce = dragForce;
 
     IEnumerator ArrowMovement(){
-
+        
 
         yield return null;
     }
     
     Vector3 PositionAtQuadraticBezierCurve(float t, Vector3 p1, Vector3 p2, Vector3 p3) => (((1 - t) * (1 - t)) * p1) + (2 * (1 - t) * t * p2) + (t * t) * p3;
-    
+
     void OnDrawGizmos() {
         Gizmos.color = new Color(.5f,0,1,0.5f);
-
+        /*
         Gizmos.DrawSphere(PositionAtQuadraticBezierCurve(0, transform.position * teste.x, transform.position + (transform.forward * teste.y), transform.position + (transform.forward * teste.z) - (transform.up * teste.w) ), 0.2f);
         Gizmos.DrawSphere(PositionAtQuadraticBezierCurve(.1f, transform.position * teste.x, transform.position + (transform.forward * teste.y), transform.position + (transform.forward * teste.z) - (transform.up * teste.w) ), 0.2f);
         Gizmos.DrawSphere(PositionAtQuadraticBezierCurve(.2f, transform.position * teste.x, transform.position + (transform.forward * teste.y), transform.position + (transform.forward * teste.z) - (transform.up * teste.w) ), 0.2f);
@@ -53,9 +67,6 @@ public class BowProjectile : MonoBehaviour{
         Gizmos.DrawSphere(PositionAtQuadraticBezierCurve(.8f, transform.position * teste.x, transform.position + (transform.forward * teste.y), transform.position + (transform.forward * teste.z) - (transform.up * teste.w) ), 0.2f);
         Gizmos.DrawSphere(PositionAtQuadraticBezierCurve(.9f, transform.position * teste.x, transform.position + (transform.forward * teste.y), transform.position + (transform.forward * teste.z) - (transform.up * teste.w) ), 0.2f);
         Gizmos.DrawSphere(PositionAtQuadraticBezierCurve(1f, transform.position * teste.x, transform.position + (transform.forward * teste.y), transform.position + (transform.forward * teste.z) - (transform.up * teste.w) ), 0.2f);
-    }
-    public void ApplyArrowForce(Vector3 arrowDirection, float arrowStartForce, float dragForce){
-        rigidbody.velocity = arrowDirection.normalized * arrowStartForce;
-        rigidbody.drag = dragForce;
+        */
     }
 }

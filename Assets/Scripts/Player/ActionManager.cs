877469e150f07fcfa48327d9d06d356f52ae7539
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-07-05 16:18:49 -0300

    Cleaning coments

--- Assets/Scripts/Player/ActionManager.cs
+++ Assets/Scripts/Player/ActionManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class ActionManager : MonoBehaviour{
-    //InputActionManager playerInputManager;
     InputActionManager PlayerInputManager { get{ return PlayerManager.instance.InputActionManager; } }
 
     public void ActionButtonBehaviour(object sender, InputAction.CallbackContext buttonContext){

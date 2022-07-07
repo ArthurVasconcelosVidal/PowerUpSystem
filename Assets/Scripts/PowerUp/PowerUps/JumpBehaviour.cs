e0dfcdcb05c4837589408f1de74b52a56fe23af8
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-07-07 08:52:22 -0300

    subscribing in the input system

--- Assets/Scripts/PowerUp/PowerUps/JumpBehaviour.cs
+++ Assets/Scripts/PowerUp/PowerUps/JumpBehaviour.cs
@@ -1,30 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public struct JumpFeature{
-    public float maxJumpTime;
-    public float maxJumpHeight;
-    public float timeToApex; 
-    public float jumpGravity; 
-    public float iniJumpVelocity;
+    public float MaxJumpTime { get; }
+    public float MaxJumpHeight { get; }
+    public float TimeToApex { get; } 
+    public float JumpGravity { get; } 
+    public float IniJumpVelocity { get; }
 
     public JumpFeature(float maxJumpTime, float maxJumpHeight){
-        this.maxJumpTime = maxJumpTime;
-        this.maxJumpHeight = maxJumpHeight;
-        timeToApex = maxJumpTime / 2;
-        jumpGravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToApex, 2);
-        iniJumpVelocity = (2 * maxJumpHeight) / timeToApex;
+        MaxJumpTime = maxJumpTime;
+        MaxJumpHeight = maxJumpHeight;
+        TimeToApex = maxJumpTime / 2;
+        JumpGravity = (-2 * maxJumpHeight) / Mathf.Pow(TimeToApex, 2);
+        IniJumpVelocity = (2 * maxJumpHeight) / TimeToApex;
     }
 }
 
 public class JumpBehaviour : MonoBehaviour{
+    InputActionManager PlayerInputManager { get{ return PlayerManager.instance.InputActionManager; } }
+
+    JumpFeature[] jumpList = new JumpFeature[3];
+    void Start() {
+        
+    }
+
+    void Jump(object sender, InputAction.CallbackContext buttonContext){
+        Debug.Log("jump");
+    }
 
     void OnEnable() {
-
+        //Debug.Log($"maxJumpTime: {teste.MaxJumpTime} \n maxJumpHeight: {teste.MaxJumpHeight} \n timeToApex: {teste.TimeToApex} \n jumpGravity: {teste.JumpGravity} \n iniJumpVelocity: {teste.IniJumpVelocity}");
+        PlayerInputManager.OnSouthButtonPerformed += Jump;
     }
 
     void OnDisable() {
-        
+        PlayerInputManager.OnSouthButtonPerformed -= Jump;
     }
 }

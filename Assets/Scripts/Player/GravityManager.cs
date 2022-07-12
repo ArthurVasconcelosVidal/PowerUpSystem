b3fe9abf01b742a2475d17e0680fced6f7650f1a
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-07-12 15:49:57 -0300

    Adding base and falling gravity forces

--- Assets/Scripts/Player/GravityManager.cs
+++ Assets/Scripts/Player/GravityManager.cs
@@ -6,20 +6,40 @@ using UnityEngine;
 public class GravityManager : MonoBehaviour{
     
     PlayerManager PlayerManager { get{ return PlayerManager.instance; } }
-
     [SerializeField] GravityType gravityType = GravityType.onGrounded;
-    [SerializeField] string groundTag;
     [SerializeField] float gravityForce;
     Vector3 gravityDirection = Vector3.down;
+    bool isGrounded = false;
 
+    public bool IsGrounded  { get => isGrounded;
+                                set{
+                                    isGrounded = value;
+                                    GravityBehaviour();
+                                    }
+                            }
     public float GravityForce { get => gravityForce; set{ gravityForce = value; } }
     public Vector3 GravityDirection { get => gravityDirection; set{ gravityDirection = value; } }
+
+    void Start() {
+        GravityBehaviour();    
+    }
 
     void FixedUpdate() {
         GravityApply();
     }
 
     public void GravityTypeSelector(GravityType gravityType) => this.gravityType = gravityType;
+
+    void GravityBehaviour(){
+            if(IsGrounded){
+                const float BASE_GRAVITY_FORCE = 2f;
+                gravityForce = BASE_GRAVITY_FORCE;
+            }
+            else{
+                const float FALLING_GRAVITY_FORCE = 9.8f;
+                gravityForce = FALLING_GRAVITY_FORCE;
+            }
+    }
 
     void GravityApply() => PlayerManager.CharacterRigidbody.velocity = gravityDirection * gravityForce;
 
--- Assets/Scripts/Player/GroundCollider.cs
+++ Assets/Scripts/Player/GroundCollider.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundCollider : MonoBehaviour{
+    GravityManager GravityManager { get => PlayerManager.instance.GravityManager; }
+
     void OnTriggerEnter(Collider other) {
-        Debug.Log("ENTRO");
+        GravityManager.IsGrounded = true;
     }
 
     void OnTriggerExit(Collider other) {
-        Debug.Log("SAIU");
+        GravityManager.IsGrounded = false;
     }
 }

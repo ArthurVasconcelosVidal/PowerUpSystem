ba5201c92fc3193e02b32c0b2dcdb03b29c2a914
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-07-12 11:14:59 -0300

    Refactoring gravity manager to be more modular

--- Assets/Scripts/Player/GravityManager.cs
+++ Assets/Scripts/Player/GravityManager.cs
@@ -1,46 +1,31 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
-
-enum GravityType{
-    onGrounded,
-    onFalling,
-    onJumping
-}
 
 public class GravityManager : MonoBehaviour{
     
     PlayerManager PlayerManager { get{ return PlayerManager.instance; } }
 
     [SerializeField] GravityType gravityType = GravityType.onGrounded;
-    [SerializeField] float distToGround = 0.1f;
-    [SerializeField] float groundDistanceOffset = 0;
     [SerializeField] string groundTag;
     [SerializeField] Vector3 gravityForce;
+    public Vector3 GravityForce { get => gravityForce; set{ gravityForce = value; } }
 
     void FixedUpdate() {
-        GravityApplication();    
+        GravityApply();
     }
 
-    void GravityApplication(){
-        switch(gravityType){
-            case GravityType.onGrounded:
-                PlayerManager.CharacterRigidbody.velocity = gravityForce;
-                break;
-            case GravityType.onFalling:
-                break;
-            case GravityType.onJumping:
-                break;
-        }
-    }
+    public void GravityTypeSelector(GravityType gravityType) => this.gravityType = gravityType;
 
-    
+    void GravityApply() => PlayerManager.CharacterRigidbody.velocity = GravityForce;
 
-    public bool IsGrounded() {
+    public bool IsGrounded(float distToGround = 0.1f, float groundDistanceOffset = 0) {
         RaycastHit hitGround;
         if (Physics.Raycast(transform.position, -transform.up, out hitGround, distToGround + 0.1f + groundDistanceOffset) && hitGround.transform.gameObject.CompareTag(groundTag))
             return true;
         else
             return false;
     }
+
 }

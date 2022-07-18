3f8ab39f3ff7785681fadd8a7786512e2b3c3b31
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-07-18 10:43:36 -0300

    Double Jump Basics

--- Assets/Scripts/PowerUp/PowerUps/Jump/DoubleJump.cs
+++ Assets/Scripts/PowerUp/PowerUps/Jump/DoubleJump.cs
@@ -1,17 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DoubleJump : MonoBehaviour{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    PlayerManager PlayerManager{ get{ return PlayerManager.instance; } }
+    [SerializeField] LayerMask groundLayer;
+    JumpFeature firstJump;
+    JumpFeature secondJump;
+    JumpFeature actualJump;
+
+    bool canDoubleJump;
+
+    void Awake(){
+        firstJump = new JumpFeature(0.5f, 4); 
+        secondJump = new JumpFeature(0.8f, 7); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Jump(object sender, InputAction.CallbackContext buttonContext){
+        if(IsGrounded()){
+            canDoubleJump = true;
+            actualJump = firstJump;
+            Test(actualJump);
+        }else if(canDoubleJump){
+            actualJump = secondJump;
+            Test(actualJump);
+            canDoubleJump = false;
+        }         
+    }
+    
+    void Test(JumpFeature jump){
+        PlayerManager.GravityManager.IsUsingSpecialGravity = true;
+        PlayerManager.GravityManager.GravityForce = -jump.JumpGravity;
+        PlayerManager.CharacterRigidbody.AddForce(jump.IniJumpVelocity * Vector3.up, ForceMode.VelocityChange);
+    }
+
+    void CancelJump(object sender, InputAction.CallbackContext buttonContext){
+        if(!IsGrounded()){
+            const float GRAVITY_FALL_MULTIPLIER = 1.8f;
+            PlayerManager.GravityManager.GravityForce = -actualJump.JumpGravity * GRAVITY_FALL_MULTIPLIER;
+        }
+    }
+
+    bool IsGrounded(float groundDistanceOffset = 0) {
+        float distToGround = transform.localScale.y; 
+        if (Physics.Raycast(transform.position, -transform.up, distToGround + 0.05f + groundDistanceOffset, groundLayer))
+            return true;
+        else
+            return false;
+    }
+
+    void OnEnable() {
+        PlayerManager.InputActionManager.OnSouthButtonPerformed += Jump;
+        PlayerManager.InputActionManager.OnSouthButtonCanceled += CancelJump;
+    }
+
+    void OnDisable() {
+        PlayerManager.InputActionManager.OnSouthButtonPerformed -= Jump;
+        PlayerManager.InputActionManager.OnSouthButtonCanceled -= CancelJump;
     }
 }

8dab6580a8ec121246f44ada80a121f3e635c575
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-07-29 10:35:04 -0300

    Removing old scripts and recreating the interaction behaviour

---
+++ Assets/Scripts/Miscellaneous/InteractiveObjects/InteractionBehahviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractionBehahviour : MonoBehaviour{
+    [SerializeField] string playerTag;
+    InputActionManager PlayerInput { get => PlayerManager.instance.InputActionManager; }
+
+    void InteractBehaviour(object sender, InputAction.CallbackContext context){
+        Debug.Log($"interagindo: {this.gameObject.name}");
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag(playerTag))
+            PlayerInput.OnSouthPrimaryButtonPerformed += InteractBehaviour;
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.CompareTag(playerTag))
+            PlayerInput.OnSouthPrimaryButtonPerformed -= InteractBehaviour;
+    }
+}

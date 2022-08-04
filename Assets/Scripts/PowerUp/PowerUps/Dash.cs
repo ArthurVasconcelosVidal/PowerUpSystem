c43c19f36c76a6878d5ed9ee4de620251d569c2f
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-08-04 11:32:14 -0300

    Adding the direction to the dash

--- Assets/Scripts/PowerUp/PowerUps/Dash.cs
+++ Assets/Scripts/PowerUp/PowerUps/Dash.cs
@@ -5,15 +5,27 @@ using UnityEngine.InputSystem;
 
 public class Dash : MonoBehaviour, IPressReleaseAction{
     [SerializeField] InputActionManager playerInput;
+    [SerializeField] float dashDistance = 4;
 
     public void OnButtonPressed(object sender, InputAction.CallbackContext buttonContext) => DashAction();
 
     public void OnButtonReleased(object sender, InputAction.CallbackContext buttonContext) => Debug.Log("Fisnish");
 
     void DashAction(){
-        Debug.Log("Funcionando");
+        Vector3 direction = ObjectRelatedDirection(playerInput.LeftStickValue, Camera.main.gameObject);
+        
+        if(direction == Vector3.zero)
+            return;
+        
+
     }
 
+    Vector3 ObjectRelatedDirection(Vector2 inputDirection, GameObject relatedObject){
+        var forwardDirection = Vector3.ProjectOnPlane(relatedObject.transform.forward, transform.up);
+        var rightDirection = Vector3.ProjectOnPlane(relatedObject.transform.right, transform.up);
+        Vector3 finalDirection = forwardDirection * inputDirection.y + rightDirection * inputDirection.x;
+        return finalDirection.normalized;
+    }
 
     void OnEnable() {
         playerInput.OnWestButtonPerformed += OnButtonPressed;

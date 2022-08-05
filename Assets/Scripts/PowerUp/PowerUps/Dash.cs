d37f6f875aae2499b339bac71b48c48dbdd9e8d2
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-08-05 11:46:04 -0300

    Implementing the dash

--- Assets/Scripts/PowerUp/PowerUps/Dash.cs
+++ Assets/Scripts/PowerUp/PowerUps/Dash.cs
@@ -2,29 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Threading.Tasks;
 
 public class Dash : MonoBehaviour, IPressReleaseAction{
     [SerializeField] InputActionManager playerInput;
-    [SerializeField] float dashDistance = 4;
+    [SerializeField] Rigidbody playerRigidBody;
+    [SerializeField] GameObject forwardReference; 
+    bool canDash = true;
+    [SerializeField] float dashSpeed = 5;
+    [SerializeField] float dashTime;
+    [SerializeField] int dashRecoveryTime = 1;
+
 
     public void OnButtonPressed(object sender, InputAction.CallbackContext buttonContext) => DashAction();
 
     public void OnButtonReleased(object sender, InputAction.CallbackContext buttonContext) => Debug.Log("Fisnish");
 
     void DashAction(){
-        Vector3 direction = ObjectRelatedDirection(playerInput.LeftStickValue, Camera.main.gameObject);
-        
-        if(direction == Vector3.zero)
+        if(!canDash)
             return;
         
-
+        DashActive(forwardReference.transform.forward.normalized, dashTime);
     }
 
-    Vector3 ObjectRelatedDirection(Vector2 inputDirection, GameObject relatedObject){
-        var forwardDirection = Vector3.ProjectOnPlane(relatedObject.transform.forward, transform.up);
-        var rightDirection = Vector3.ProjectOnPlane(relatedObject.transform.right, transform.up);
-        Vector3 finalDirection = forwardDirection * inputDirection.y + rightDirection * inputDirection.x;
-        return finalDirection.normalized;
+    async void DashActive(Vector3 direction, float dashTime) {
+        while (dashTime > 0) {
+            dashTime -= Time.fixedDeltaTime;
+            playerRigidBody.velocity = direction * dashSpeed;
+            await Task.Yield();
+        }
+
+        playerRigidBody.velocity = Vector3.zero;
+        DashReload(dashRecoveryTime);
+    }
+
+    async void DashReload(int dashRecoveryTime){
+        await Task.Delay(dashRecoveryTime * 1000);
+        canDash = true;
     }
 
     void OnEnable() {

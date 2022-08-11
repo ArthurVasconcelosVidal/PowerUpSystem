f50f7591c5580c6f5737f5d1511aef1ec53af518
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-08-11 15:29:39 -0300

    Creating Base Animation Manager script

--- Assets/Scripts/Player/AnimationManager.cs
+++ Assets/Scripts/Player/AnimationManager.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class AnimationManager : MonoBehaviour{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] Animator playerAnimator;
+
+    public void EnableRootMotion(bool enable =  true) => playerAnimator.applyRootMotion = enable;
 }

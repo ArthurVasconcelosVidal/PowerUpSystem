539193f4902c0e5ca67358ae9948b941ab7f97de
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-08-19 14:44:58 -0300

    Changing value on Animator

--- Assets/Scripts/Player/GroundCollider.cs
+++ Assets/Scripts/Player/GroundCollider.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class GroundCollider : MonoBehaviour{
     GravityManager GravityManager { get => PlayerManager.instance.GravityManager; }
+    [SerializeField] Animator playerAnimator;
 
     void OnTriggerEnter(Collider other) {
         GravityManager.IsGrounded = true;
+        if(playerAnimator) playerAnimator.SetBool("OnGround", true);
     }
 
     void OnTriggerExit(Collider other) {
         GravityManager.IsGrounded = false;
+        if(playerAnimator) playerAnimator.SetBool("OnGround", false);
     }
 }

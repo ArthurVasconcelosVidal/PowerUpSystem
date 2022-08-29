0838a0c25764cc6040508ccbff87ab8c89efe73c
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-08-29 11:14:00 -0300

    Update SpaceImpulse.cs

--- Assets/Scripts/SpecialObjects/SpaceImpulse.cs
+++ Assets/Scripts/SpecialObjects/SpaceImpulse.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Threading.Tasks;
-public class SpaceImpulse : MonoBehaviour{
+using UnityEngine.InputSystem;
+
+public class SpaceImpulse : MonoBehaviour, IPressReleaseAction{
     [SerializeField] SpaceImpulse targetSpaceImpulse;
-    [SerializeField] bool insideSpaceImpulse;
+    [SerializeField] bool usingSpaceImpulse;
     [SerializeField] string playerTag;
     [SerializeField] float velocity;
     [SerializeField] float gravityForce;
     [SerializeField] float timeToEnd;
     GravityManager playerGravity;
 
+    GameObject player;
     /*
         TODO:
             - Neutralize gravity force and apply a force to the center (Feito)
             - Disable gravity point after a certain quantity of time or when the player press a button to cancel the space impulse (Feito)
-            
-            - Disable any movement or powerUp (Make easy to disable specific inputs (like disable a button))
+            - Disable any movement or powerUp (Make easy to disable specific inputs (like disable a button)) (Feito)
+
             - Make the movement to the next object (can use spherical interpolation to move between two points)
             - Make the posibility of unlimited stop points in the movement (can make a trail if space impulse)
             - make the movement Smooth
@@ -25,42 +28,73 @@ public class SpaceImpulse : MonoBehaviour{
     
     void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag(playerTag)){
-            playerGravity = other.gameObject.GetComponent<GravityManager>();
-            ChangeGravity(other.gameObject.GetComponent<Rigidbody>());
-            insideSpaceImpulse = true;
+            usingSpaceImpulse = true;
+            player = other.gameObject;
+            playerGravity = player.GetComponent<GravityManager>();
+            ChangeGravity();
+            EnablePlayerComponents(false);
+            ButtonBehaviour(true);
             TimeToQuit();
         }
     }
 
     void OnTriggerStay(Collider other) {
-        if(other.gameObject.CompareTag(playerTag)){
+        if(other.gameObject.CompareTag(playerTag) && usingSpaceImpulse){
+            playerGravity.GravityDirection = (transform.position - other.transform.position).normalized;
         }
     }
 
     void OnTriggerExit(Collider other) {
         if(other.gameObject.CompareTag(playerTag)){
+            EnablePlayerComponents(true);
+            ButtonBehaviour(false);
             playerGravity.ResetGravity();
-            insideSpaceImpulse = false;    
+            usingSpaceImpulse = false;    
         }
     }
 
-    void EnablePlayerComponents(GameObject player, bool enable = true){
+    public void OnButtonPressed(object sender, InputAction.CallbackContext buttonContext) => QuitImpulseSpace();
 
+    public void OnButtonReleased(object sender, InputAction.CallbackContext buttonContext) => LaunchPlayer();
+
+    void EnablePlayerComponents(bool enable = true){
+        var playerInput = player.GetComponent<InputActionManager>();
+        playerInput.EnableAllControlInputs(enable);
+        playerInput.southButtonActive = true;
+        playerInput.eastButtonActive = true;
     }
 
-    void ChangeGravity(Rigidbody playerRb){
+    void ChangeGravity(){
+        var playerRb = player.GetComponent<Rigidbody>();
         playerRb.velocity = Vector3.zero;
         playerGravity.IsUsingSpecialGravity = true;
-        playerGravity.GravityDirection = (transform.position - playerRb.position).normalized;
         playerGravity.GravityForce = gravityForce;
     }
 
     async void TimeToQuit(){
         await Task.Delay((int)timeToEnd*1000);
-        if(insideSpaceImpulse){
-            playerGravity.ResetGravity();
-        }
-        
+        QuitImpulseSpace();
     }
-    
+
+    void QuitImpulseSpace(){
+        if(usingSpaceImpulse){
+            playerGravity.ResetGravity();
+            usingSpaceImpulse = false;
+        }
+    }
+
+    void LaunchPlayer(){
+        //var playerGravity = player.GetComponent<> 
+    }
+
+    void ButtonBehaviour(bool enable){
+        var playerInput = player.GetComponent<InputActionManager>();
+        if(enable){
+            playerInput.OnEastPrimaryButtonPerformed += OnButtonPressed;
+            playerInput.OnSouthPrimaryButtonPerformed += OnButtonReleased;
+        }else{
+            playerInput.OnEastPrimaryButtonPerformed -= OnButtonPressed;
+            playerInput.OnSouthPrimaryButtonPerformed -= OnButtonReleased;
+        }
+    }
 }

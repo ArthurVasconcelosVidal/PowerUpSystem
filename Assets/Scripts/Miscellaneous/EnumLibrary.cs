b3174c4d37729d383f2e7e0f29d9baa78d92a786
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-09-12 12:46:06 -0300

    Adding priority system to animations Enum

--- Assets/Scripts/Miscellaneous/EnumLibrary.cs
+++ Assets/Scripts/Miscellaneous/EnumLibrary.cs
@@ -10,12 +10,16 @@ public enum GravityType{
 }
 
 public enum Animations{
-    IdleToRunTree,
-    FallingIdle,
-    FirstJumping,
-    DoubleJumpFlip,
-    DashPose,
-    TrampolineJump,
-    FlyPose,
-    SpinPose
+    /*
+        - Each animation name correspond to a animator anim, the name has to be the same(case sensitive)
+        - Each anim has a priority number, this will tell which animation has the priority to execute
+    */
+    IdleToRunTree = 0,
+    FallingIdle = 0,
+    FirstJumping = 1,
+    DoubleJumpFlip = 1,
+    DashPose = 1,
+    TrampolineJump = 1,
+    FlyPose = 3,
+    SpinPose = 2
 }

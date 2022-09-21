da64a6c9d389984f460cfca37b6e363977ea3b0d
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-09-21 13:01:28 -0300

    Fixing name

--- Assets/Scripts/Miscellaneous/StructLibrary.cs
+++ Assets/Scripts/Miscellaneous/StructLibrary.cs
@@ -28,7 +28,7 @@ namespace StructLibrary{
             {Animations.TrampolineJump,1},
             {Animations.FlyPose,3},
             {Animations.SpinPose,2},
-            {Animations.StrikeFowardKick, 2}
+            {Animations.StrikeForwardKick, 2}
         };
     }
 

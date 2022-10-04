49790405e621021d40d24e5c61142ce619d2a952
Tutu <arthurvvidal@gmail.com>
2022-10-04 11:12:47 -0300

    Adding new camera funcionality

--- Assets/Scripts/Player/CameraManager.cs
+++ Assets/Scripts/Player/CameraManager.cs
@@ -2,22 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.InputSystem;
 
-public class CameraManager : MonoBehaviour{
+public class CameraManager : MonoBehaviour, IPressReleaseAction{
+
     [SerializeField] CinemachineFreeLook freeLookCM;
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
     [SerializeField] float camSpeedY = 1; //Default
     [SerializeField] float camSpeedX = 80; //Default
 
-    PlayerManager PlayerManager { get {return PlayerManager.instance; } }
+    InputActionManager InputActionManager { get {return PlayerManager.instance.InputActionManager; } }
 
     void FixedUpdate(){
-        CameraMovement();
+        if(freeLookCM.enabled) CameraMovement();
     }
 
     void CameraMovement(){
         if(PlayerManager.instance.InputActionManager.RightStickValue != Vector2.zero){
-            freeLookCM.m_XAxis.Value += PlayerManager.InputActionManager.RightStickValue.x * camSpeedX * Time.fixedDeltaTime;
-            freeLookCM.m_YAxis.Value += PlayerManager.InputActionManager.RightStickValue.y * camSpeedY * Time.fixedDeltaTime;
+            freeLookCM.m_XAxis.Value += InputActionManager.RightStickValue.x * camSpeedX * Time.fixedDeltaTime;
+            freeLookCM.m_YAxis.Value += InputActionManager.RightStickValue.y * camSpeedY * Time.fixedDeltaTime;
         }
     }
+
+    void SwtichCamera(bool state){
+        freeLookCM.gameObject.SetActive(state);
+        virtualCamera.gameObject.SetActive(!state);
+    }
+    
+    public void OnButtonPressed(object sender, InputAction.CallbackContext buttonContext) => SwtichCamera(false);
+
+    public void OnButtonReleased(object sender, InputAction.CallbackContext buttonContext) => SwtichCamera(true);
+
+    void OnEnable() {
+        InputActionManager.OnRightTriggerButtonPerformed += OnButtonPressed;
+        InputActionManager.OnRightTriggerButtonCanceled += OnButtonReleased;  
+    }
+
+    void OnDisable() {
+        InputActionManager.OnRightTriggerButtonPerformed -= OnButtonPressed;
+        InputActionManager.OnRightTriggerButtonCanceled -= OnButtonReleased; 
+    }
+
 }

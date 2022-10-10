2a04f2c98d3a6305f13ad206440a482f2fe64e66
Tutu <arthurvvidal@gmail.com>
2022-10-10 11:34:23 -0300

    Starting interest system

---
+++ Assets/Scripts/Player/InterestManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterestManager : MonoBehaviour{
+    [SerializeField] GameObject[] interestObjects = new GameObject[8];
+
+    void GetInInterest(){
+
+    }
+
+    GameObject GetClosesObject(){
+        
+    }
+}

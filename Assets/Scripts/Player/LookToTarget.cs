cff032bdeaff36bb2378457c18d9b91b068f4892
Tutu <arthurvvidal@gmail.com>
2022-10-21 10:48:34 -0300

    Finishing interest System

--- Assets/Scripts/Player/LookToTarget.cs
+++ Assets/Scripts/Player/LookToTarget.cs
@@ -7,51 +7,37 @@ using System.Threading.Tasks;
 public class LookToTarget : MonoBehaviour{
     [SerializeField] InterestManager interestManager;
     [SerializeField] GameObject MeshObject {get => PlayerManager.instance.MeshObject;}
-    [SerializeField] MultiAimConstraint headAim;
     [SerializeField] GameObject headAimObject;
     [SerializeField] GameObject baseLookPosition;
+    bool rightAngle;
     [SerializeField] bool isLooking = false;
-    bool isReturningToNormal;
-    [SerializeField] bool lookState = true;
+    [SerializeField] bool isMovingToPoint = false;
+    [SerializeField] bool isMovingToOrigin = false;
     [SerializeField] float speed;
-
-    //IEnumerator moveToPoint, moveToOrigin;
-
+    
     void OnClosestObjectBehavior(object sender, EventArgs args) => StartLookAt();
-
-    void Awake() {
-       // moveToPoint = MoveToPoint();
-        //moveToOrigin = MoveToOrigin();    
-    }
 
     void StartLookAt(){
         if(!isLooking){
             isLooking = true;
-            MoveAim();
+            SetIsLooking();
+            MoveToPoint();
         }
     }
-    
-    //Fix this
-    async void MoveAim(){
-        while (interestManager.ClosestObject){
-            bool rightAngle = LookInRightAngle();
-            IEnumerator moveToPoint = MoveToPoint();
-            IEnumerator moveToOrigin = MoveToOrigin();   
-            if(rightAngle && lookState){
-                lookState = false;
-                StopCoroutine(moveToOrigin);
-                StartCoroutine(moveToPoint);
-                print("Olhando pro ponto");
-            }else if(!rightAngle && !lookState){
-                lookState = true;
-                StopCoroutine(moveToPoint);
-                StartCoroutine(moveToOrigin);
-                print("Olhando pra origem");
-            }
-            await Task.Yield();
+
+    async void SetIsLooking(){
+        const float SECONDS = 0.5f;
+        await Task.Delay((int)(SECONDS * 1000));
+        if(interestManager.ClosestObject) isLooking = true;
+        else isLooking = false; 
+        
+        if(isLooking) {
+            rightAngle = LookInRightAngle();
+            SetIsLooking();
+            if(!isMovingToPoint && rightAngle) MoveToPoint();
+        }else if(headAimObject.transform.position != baseLookPosition.transform.position && !isMovingToOrigin){
+            MoveToOrigin();
         }
-        StartCoroutine(MoveToOrigin());
-        isLooking = false;
     }
 
     bool LookInRightAngle(){
@@ -63,23 +49,26 @@ public class LookToTarget : MonoBehaviour{
             return false;
     }
 
-    IEnumerator MoveToPoint(){
-        while (interestManager.ClosestObject){
+    async void MoveToPoint(){
+        isMovingToPoint = true;
+        while (interestManager.ClosestObject && isLooking && rightAngle){
             var direction = interestManager.ClosestObject.transform.position - headAimObject.transform.position;
             if(direction.sqrMagnitude > 0.05) headAimObject.transform.position += direction.normalized * speed * Time.fixedDeltaTime;
-            yield return null;
+            await Task.Yield();
         }
+        isMovingToPoint = false;
     }
 
-    IEnumerator MoveToOrigin(){
+    async void MoveToOrigin(){
         float percent = 0;
         Vector3 startPosition = headAimObject.transform.position;
-        while (percent < 1){
-            Debug.Log("here");
+        isMovingToOrigin = true;
+        while (percent < 1 && (!isLooking || !rightAngle)){
             percent += Time.fixedDeltaTime * speed;
             headAimObject.transform.position = Vector3.Lerp(startPosition, baseLookPosition.transform.position, percent);
-            yield return null;
+            await Task.Yield();
         }
+        isMovingToOrigin = false;
     }
 
     void OnEnable() {
